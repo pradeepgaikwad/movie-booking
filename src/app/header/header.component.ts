d16011e912c771677c53220f5c0de08b50de578e
DESKTOP-35QV3F8\admin <pradeepgaikwad003@gmail.com>
2022-04-12 18:55:35 +0530

    added log out button and corresponding functiona;ity

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../service/authentication.service';
+import { HttpAPIService } from '../service/http-api.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpAPIService,
+    private authService: AuthenticationService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  logout(){
+    console.log('Inside Logout');
+    this.authService.userLoggedOut();
+    this.http.postLogout();
+    this.router.navigate(['login']);
+  }
+
+  isUserLoggedIn(){
+    return this.authService.isUserLoggedIn();
+  }
 }
