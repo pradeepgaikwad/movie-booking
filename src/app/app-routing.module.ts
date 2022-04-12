af704d5b3bbca1eb848bd49036ea46accc55e0c4
DESKTOP-35QV3F8\admin <pradeepgaikwad003@gmail.com>
2022-04-12 18:57:34 +0530

    added can activate functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RouteGuardService } from './service/route-guard.service';
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent,canActivate: [RouteGuardService]}
 ];
 
 @NgModule({
